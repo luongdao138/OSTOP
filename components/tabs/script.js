a47f5b71a7268268a8ce104300d855994900290e
truongVX85024 <truongvx85024@co-well.vn>
2022-07-08 17:49:11 +0700

    add UI sp online store and switching tab function

---
+++ components/tabs/script.js
@@ -0,0 +1,26 @@
+function Tabs() {
+  var bindAll = function() {
+    var tabs = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < tabs.length ; i++) {
+      tabs[i].addEventListener('click', change, false);
+    }
+  }
+
+  var clear = function() {
+    var tabs = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < tabs.length ; i++) {
+      tabs[i].classList.remove('active-tab');
+      var id = tabs[i].getAttribute('data-tab');
+      document.getElementById(id).classList.remove('active-tab');
+    }
+  }
+
+  var change = function(e) {
+    clear();
+    e.target.classList.add('active-tab');
+    var id = e.currentTarget.getAttribute('data-tab');
+    document.getElementById(id).classList.add('active-tab');
+  }
+
+  bindAll();
+}
