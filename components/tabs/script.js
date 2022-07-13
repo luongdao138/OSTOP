c9a22db2f919c85ce392f2acb5507b59257e0154
truongVX85024 <truongvx8944@co-well.vn>
2022-07-13 16:03:35 +0700

    update online store responsive SP

--- components/tabs/script.js
+++ components/tabs/script.js
@@ -1,23 +1,23 @@
 function Tabs() {
-  var bindAll = function() {
+  var bindAll = function () {
     var tabs = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < tabs.length ; i++) {
+    for (var i = 0; i < tabs.length; i++) {
       tabs[i].addEventListener('click', change, false);
     }
   }
 
-  var clear = function() {
+  var clear = function () {
     var tabs = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < tabs.length ; i++) {
+    for (var i = 0; i < tabs.length; i++) {
       tabs[i].classList.remove('active-tab');
       var id = tabs[i].getAttribute('data-tab');
       document.getElementById(id).classList.remove('active-tab');
     }
   }
 
-  var change = function(e) {
+  var change = function (e) {
     clear();
-    e.target.classList.add('active-tab');
+    e.currentTarget.classList.add('active-tab');
     var id = e.currentTarget.getAttribute('data-tab');
     document.getElementById(id).classList.add('active-tab');
   }
