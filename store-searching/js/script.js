28b250ae64f8354101dcd067cf6894e9a562aa80
Luong Dao <luongdv85025@co-well.vn>
2022-07-28 01:10:03 +0700

    fix ios scroll

--- store-searching/js/script.js
+++ store-searching/js/script.js
@@ -1,22 +1,38 @@
-const modalOverlay = document.querySelector('.modal__overlay')
-const modalWrapper = document.querySelector('.modal__wrapper')
+function syncHeight() {
+  document.documentElement.style.setProperty(
+    '--window-inner-height',
+    `${window.innerHeight}px`
+  );
+}
 
-const btnModal = document.querySelector('.searching-box__btn')
-const closeModalBtn = document.querySelector('.modal__close-btn')
+syncHeight();
+
+window.addEventListener('resize', syncHeight);
+
+const modalOverlay = document.querySelector('.modal__overlay');
+const modalWrapper = document.querySelector('.modal__wrapper');
+
+const btnModal = document.querySelector('.searching-box__btn');
+const closeModalBtn = document.querySelector('.modal__close-btn');
 
 function openModal() {
-        modalOverlay.classList.add('show')
-        modalWrapper.classList.add('slide-in')
+  modalOverlay.classList.add('show');
+  modalWrapper.classList.add('slide-in');
 
-        document.body.style.overflow = 'hidden'
+  document.documentElement.classList.add('is-locked');
 }
 
 function closeModal() {
-    modalOverlay.classList.remove('show')
-    modalWrapper.classList.remove('slide-in')
-    document.body.style.overflow = 'visible'
+  modalOverlay.classList.remove('show');
+  modalWrapper.classList.remove('slide-in');
+
+  document.documentElement.classList.remove('is-locked');
 }
 
-btnModal.addEventListener('click', openModal)
-closeModalBtn.addEventListener('click', closeModal)
-modalOverlay.addEventListener('click', closeModal)
+btnModal.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
+
+document
+  .querySelector('.modal__content')
+  .style.setProperty('--window-inner-height', `${window.innerHeight}px`);
